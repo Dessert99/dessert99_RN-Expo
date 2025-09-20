c7c6f5d5a9db6beea19037068ab2a6b39bfc719b
Dessert99 <lsjh1234@naver.com>
2025-09-21 04:26:08 +0900

    Feat: 벡터 아이콘 적용

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { colors } from "@/constants";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 import React from "react";
 
@@ -13,18 +14,45 @@ export default function TabLayout() {
         name='index'
         options={{
           title: "홈",
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <Ionicons
+                name={focused ? "home-sharp" : "home-outline"} // 포커스 유무에 따라 아이콘 변경
+                size={24}
+                color={color} // 이러면 tabBarActiveTintColor 적용
+              />
+            );
+          },
         }}
       />
       <Tabs.Screen
         name='my' // 경로
         options={{
           title: "내 프로필",
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <Ionicons
+                name={focused ? "person-circle" : "person-circle-outline"}
+                size={24}
+                color={color}
+              />
+            );
+          },
         }}
       />
       <Tabs.Screen
         name='setting'
         options={{
           title: "설정",
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <Ionicons
+                name={focused ? "settings-sharp" : "settings-outline"}
+                size={24}
+                color={color}
+              />
+            );
+          },
         }}
       />
     </Tabs>
