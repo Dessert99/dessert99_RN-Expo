b17a0ad9449e211bde55105e8e525199e029d7bf
Dessert99 <lsjh1234@naver.com>
2025-09-21 04:28:47 +0900

    Chore: tsx 한 줄 암시적 반환으로 변경

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -14,45 +14,40 @@ export default function TabLayout() {
         name='index'
         options={{
           title: "홈",
-          tabBarIcon: ({ color, focused }) => {
-            return (
-              <Ionicons
-                name={focused ? "home-sharp" : "home-outline"} // 포커스 유무에 따라 아이콘 변경
-                size={24}
-                color={color} // 이러면 tabBarActiveTintColor 적용
-              />
-            );
-          },
+          tabBarIcon: ({ color, focused }) => (
+            //암시적 반환 정석
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"} // 포커스 유무에 따라 아이콘 변경
+              size={24}
+              color={color} // 이러면 tabBarActiveTintColor 적용
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name='my' // 경로
         options={{
           title: "내 프로필",
-          tabBarIcon: ({ color, focused }) => {
-            return (
-              <Ionicons
-                name={focused ? "person-circle" : "person-circle-outline"}
-                size={24}
-                color={color}
-              />
-            );
-          },
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person-circle" : "person-circle-outline"}
+              size={24}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name='setting'
         options={{
           title: "설정",
-          tabBarIcon: ({ color, focused }) => {
-            return (
-              <Ionicons
-                name={focused ? "settings-sharp" : "settings-outline"}
-                size={24}
-                color={color}
-              />
-            );
-          },
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "settings-sharp" : "settings-outline"}
+              size={24}
+              color={color}
+            />
+          ),
         }}
       />
     </Tabs>
