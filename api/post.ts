671e8ecc01917888c59e4c841dc84548b6f98c1c
Dessert99 <lsjh1234@naver.com>
2025-10-01 10:41:03 +0900

    Feat: 게시글 요청 api 함수

--- api/post.ts
+++ api/post.ts
@@ -1,10 +1,33 @@
-import { CreatePostDto } from "@/types";
+import { CreatePostDto, Post } from "@/types";
 import { instance } from "./instance";
 
-async function createPost(body: CreatePostDto) {
+export async function createPost(body: CreatePostDto) {
   const { data } = await instance.post("/posts", body);
 
   return data;
 }
 
-export { createPost };
+// 게시글 요청 로직 : 파라미터-페이지, 리턴 타입 - Post배열
+export async function getPosts(page = 1): Promise<Post[]> {
+  const { data } = await instance.post(`/posts?page=${page}`);
+  return data;
+}
+
+/* 이 포스트의 배열이 리턴 값.
+interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  createdAt: string;
+  author: User;
+  imageUris: ImageUri[];
+  likes: { userId: number }[];
+  hasVote: boolean;
+  voteCount: number;
+  commentCount: number;
+  viewCount: number;
+  votes?: PostVote[];
+  comments?: PostComment[];
+}
+*/
