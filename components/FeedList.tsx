9b683c50a5bf6b059c446e46ee558e4e750df2ac
Dessert99 <lsjh1234@naver.com>
2025-10-02 13:51:15 +0900

    feat(infinitePosts): 피드 무한 스크롤 적용.

--- components/FeedList.tsx
+++ components/FeedList.tsx
@@ -5,14 +5,32 @@ import { FlatList, StyleSheet } from "react-native";
 import FeedItem from "./FeedItem";
 
 function FeedList() {
-  const { data: posts, error } = useGetInfinitePosts(); // data의 이름을 posts로 변경
+  const {
+    data: posts,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useGetInfinitePosts(); // data의 이름을 posts로 변경
   if (error) console.log("[FeedList] fetch error:", error); // ← 여기 한 줄
+
+  //다음 페이지 가져오는 로직
+  const handleEndReached = () => {
+    // 다음 페이지가 있으면서 다음 페이지를 가져오는 중이 아닐 때만 다음 페이지를 가져온다.
+    if (hasNextPage && !isFetchingNextPage) {
+      console.log("다음 페이지 ->", (posts?.pages.length ?? 0) + 1);
+      fetchNextPage(); // 다음 페이지
+    }
+  };
+
   return (
     <FlatList
       data={posts?.pages.flat()} // 게시글 배열의 배열로 온다. ex) [게시글1, 게시글2 ...게시글10], [게시글11, 게시글12...게시글20]
       renderItem={({ item }) => <FeedItem post={item} />} // data 배열 요소를 하나씩 FeedItem에 넣음
       contentContainerStyle={styles.contentContainer} // FlatList 스타일
       keyExtractor={(item) => String(item.id)} //FlatList의 key 값은 문자열이어야 한다.
+      onEndReached={handleEndReached} // 다음 글 목록 불러오는 옵션
+      onEndReachedThreshold={0.5} // 밑에 완전히 닫지 않아도 자동으로 불러오는 옵션
     />
   );
 }
