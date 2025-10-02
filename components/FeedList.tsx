f6d3916eeaa4205dfe0129d4a04f1dcb5194b610
Dessert99 <lsjh1234@naver.com>
2025-10-02 13:30:38 +0900

    feat(feedlist): 더미데이터 제거하고 서버에서 피드 목록 요청

--- components/FeedList.tsx
+++ components/FeedList.tsx
@@ -1,57 +1,15 @@
 import { colors } from "@/constants";
+import { useGetInfinitePosts } from "@/hooks/queries/useGetInfinitePosts";
 import React from "react";
 import { FlatList, StyleSheet } from "react-native";
 import FeedItem from "./FeedItem";
 
-const dummyData = [
-  {
-    id: 1,
-    userId: 1,
-    title: "더미 타이틀",
-    description:
-      "더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음",
-    createdAt: "2024-02-02",
-    author: {
-      id: 1,
-      nickname: "닉네임",
-      imageUri: "",
-    },
-    imageUris: [],
-    likes: [],
-    hasVote: false,
-    voteCount: 1,
-    commentCount: 1,
-    viewCount: 1,
-    votes: [],
-    comments: [],
-  },
-  {
-    id: 2,
-    userId: 1,
-    title: "더미 타이틀",
-    description:
-      "더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음더미짱많음",
-    createdAt: "2025-09-30",
-    author: {
-      id: 1,
-      nickname: "닉네임",
-      imageUri: "",
-    },
-    imageUris: [],
-    likes: [],
-    hasVote: false,
-    voteCount: 1,
-    commentCount: 1,
-    viewCount: 1,
-    votes: [],
-    comments: [],
-  },
-];
-
 function FeedList() {
+  const { data: posts, error } = useGetInfinitePosts(); // data의 이름을 posts로 변경
+  if (error) console.log("[FeedList] fetch error:", error); // ← 여기 한 줄
   return (
     <FlatList
-      data={dummyData}
+      data={posts?.pages.flat()} // 게시글 배열의 배열로 온다. ex) [게시글1, 게시글2 ...게시글10], [게시글11, 게시글12...게시글20]
       renderItem={({ item }) => <FeedItem post={item} />} // data 배열 요소를 하나씩 FeedItem에 넣음
       contentContainerStyle={styles.contentContainer} // FlatList 스타일
       keyExtractor={(item) => String(item.id)} //FlatList의 key 값은 문자열이어야 한다.
