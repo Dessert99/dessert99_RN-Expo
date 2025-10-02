7c973e2bfa490445b5b82f767de2a8709f0c773d
Dessert99 <lsjh1234@naver.com>
2025-10-02 13:36:25 +0900

    feat(createPost): 새 글 올리면 리패치 로직

--- hooks/queries/useCreatePost.ts
+++ hooks/queries/useCreatePost.ts
@@ -1,4 +1,6 @@
 import { createPost } from "@/api/post";
+import { queryClient } from "@/api/queryClient";
+import { queryKeys } from "@/constants";
 import { useMutation } from "@tanstack/react-query";
 import { router } from "expo-router";
 
@@ -7,6 +9,10 @@ function useCreatePost() {
     mutationFn: createPost,
     onSuccess: () => {
       router.replace("/");
+      // 쿼리 무효화하고 상태 최신화. 쿼리가 오래되었다는 것을 판단하고 다시 리패치할 때 사용한다.
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
+      });
     },
   });
 }
