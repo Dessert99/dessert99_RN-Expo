da6e3bb32bfe3862e4ec0d3550cdb1e92ca61756
Dessert99 <lsjh1234@naver.com>
2025-10-13 02:39:03 +0900

    feat(stack): 스택에 게시글 수정 적용

--- app/post/_layout.tsx
+++ app/post/_layout.tsx
@@ -1,6 +1,6 @@
-import { Link, Stack } from "expo-router";
 import { colors } from "@/constants";
 import { Feather } from "@expo/vector-icons";
+import { Link, router, Stack } from "expo-router";
 
 export default function PostLayout() {
   return (
@@ -10,17 +10,37 @@ export default function PostLayout() {
         contentStyle: {
           backgroundColor: colors.WHITE,
         },
-      }}
-    >
+      }}>
       <Stack.Screen
-        name="write"
+        name='write'
         options={{
           title: "글쓰기",
           headerShown: true,
           headerLeft: () => (
-            <Link href={"/"} replace>
-              <Feather name="arrow-left" size={28} color={"black"} />
+            <Link
+              href={"/"}
+              replace>
+              <Feather
+                name='arrow-left'
+                size={28}
+                color={"black"}
+              />
             </Link>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name='update/[id]'
+        options={{
+          title: "수정",
+          headerShown: true,
+          headerLeft: () => (
+            <Feather
+              name='arrow-left'
+              size={28}
+              color={"black"}
+              onPress={() => router.back()}
+            />
           ),
         }}
       />
