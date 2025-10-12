0b14c029293c60bb3390bee554f6ceb531bc132b
Dessert99 <lsjh1234@naver.com>
2025-10-13 00:42:31 +0900

    feat(post): 프로필에 action-sheet 옵션 추가. - 옵션 버튼을 누르면 삭제, 수정, 취소 중 택1을 할 수 있습니다

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 "@react-navigation/native";
 import { queryClient } from "@/api/queryClient";
 import { useAuth } from "@/hooks/queries/useAuth";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { useEffect } from "react";
@@ -13,10 +14,12 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RootNavigator />
-      <Toast />
-    </QueryClientProvider>
+    <ActionSheetProvider>
+      <QueryClientProvider client={queryClient}>
+        <RootNavigator />
+        <Toast />
+      </QueryClientProvider>
+    </ActionSheetProvider>
   );
 }
 
--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -1,6 +1,7 @@
 import { colors } from "@/constants";
 import { useAuth } from "@/hooks/queries/useAuth";
 import { Post } from "@/types";
+import { useActionSheet } from "@expo/react-native-action-sheet";
 import Entypo from "@expo/vector-icons/Entypo";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Pressable, StyleSheet, Text, View } from "react-native";
@@ -12,9 +13,33 @@ interface FeedItemProps {
 
 function FeedItem({ post }: FeedItemProps) {
   const { auth } = useAuth();
+  const { showActionSheetWithOptions } = useActionSheet();
 
   const likeUsers = post.likes?.map((like) => Number(like.userId)); // 게시글에 좋아요가 있다면, 유저의 아이디만 뽑아준다.
   const isLiked = likeUsers?.includes(Number(auth.id)); // 좋아요 누른 사람 목록에 내 id가 있다면 true
+
+  // 프로필 옵션 버튼 핸들러
+  const handlePressOption = () => {
+    const options = ["삭제", "수정", "취소"];
+    const cancelButtonIndex = 2;
+    const destructiveButtonIndex = 0;
+
+    showActionSheetWithOptions(
+      { options, cancelButtonIndex, destructiveButtonIndex },
+      (selectedIndex?: number) => {
+        switch (selectedIndex) {
+          case destructiveButtonIndex: // 삭제
+            break;
+          case 1: // 수정
+            break;
+          case cancelButtonIndex: //취소
+            break;
+          default:
+            break;
+        }
+      }
+    ); // options로 어떤 버튼인지 넣을 수 있다. 그 다음 누른 버튼에 따라 핸들링하면 된다.
+  };
 
   return (
     <View style={styles.container}>
@@ -24,6 +49,17 @@ function FeedItem({ post }: FeedItemProps) {
           createdAt={post.createdAt}
           imageUri={post.author.imageUri}
           onPress={() => {}}
+          options={
+            // 내가 올린 게시글에만 옵션이 보이도록 설정
+            auth.id === post.author.id && (
+              <Ionicons
+                name='ellipsis-vertical'
+                size={24}
+                color={colors.BLACK}
+                onPress={handlePressOption}
+              />
+            )
+          }
         />
         <Text style={styles.title}>{post.title}</Text>
         <Text
