a603f792d18011d5e354f69b056f3f3d1919a9f1
Dessert99 <lsjh1234@naver.com>
2025-10-14 20:45:34 +0900

    feat(post): 상세 피드 입력창 연결

--- app/post/[id].tsx
+++ app/post/[id].tsx
@@ -1,8 +1,11 @@
 import AuthRoute from "@/components/AuthRoute";
 import FeedItem from "@/components/FeedItem";
+import InputField from "@/components/InputField";
+import { colors } from "@/constants";
 import { useGetPost } from "@/hooks/queries/useGetPost";
 import { useLocalSearchParams } from "expo-router";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function PostDetailScreen() {
   const { id } = useLocalSearchParams();
@@ -15,12 +18,31 @@ export default function PostDetailScreen() {
   return (
     <AuthRoute>
       <View style={styles.container}>
-        <ScrollView>
-          <FeedItem
-            post={post}
-            isDetail={true}
-          />
-        </ScrollView>
+        <KeyboardAwareScrollView
+          contentContainerStyle={styles.awareScrollViewContainer}>
+          <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+            <View style={{ marginTop: 12 }}>
+              <FeedItem
+                post={post}
+                isDetail={true}
+              />
+              <Text style={styles.commentCount}>
+                댓글 {post.commentCount}개
+              </Text>
+            </View>
+          </ScrollView>
+
+          {/* 댓글 인풋 */}
+          <View style={styles.commentInputContainer}>
+            <InputField
+              righteChild={
+                <Pressable style={styles.inputButtonContainer}>
+                  <Text style={styles.inputButtonText}>등록</Text>
+                </Pressable>
+              }
+            />
+          </View>
+        </KeyboardAwareScrollView>
       </View>
     </AuthRoute>
   );
@@ -29,5 +51,38 @@ export default function PostDetailScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.WHITE,
+  },
+  scrollViewContainer: {
+    backgroundColor: colors.GRAY_300,
+  },
+  commentCount: {
+    marginTop: 12,
+    backgroundColor: colors.WHITE,
+    padding: 15,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  awareScrollViewContainer: {
+    flex: 1,
+    borderTopColor: colors.GRAY_200,
+  },
+  commentInputContainer: {
+    position: "absolute",
+    bottom: 0,
+    padding: 16,
+    backgroundColor: colors.WHITE,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: colors.GRAY_200,
+    width: "100%",
+  },
+  inputButtonContainer: {
+    padding: 8,
+    backgroundColor: colors.ORANGE_600,
+    borderRadius: 5,
+  },
+  inputButtonText: {
+    color: colors.WHITE,
+    fontWeight: "bold",
   },
 });
