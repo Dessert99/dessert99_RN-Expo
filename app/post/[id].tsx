956862324530d17ef3c34e0ea0f2675c3220aebe
Dessert99 <lsjh1234@naver.com>
2025-10-15 09:56:47 +0900

    feat(post): 댓글 생성 쿼리훅 상세 페이지에 연결

--- app/post/[id].tsx
+++ app/post/[id].tsx
@@ -2,18 +2,32 @@ import AuthRoute from "@/components/AuthRoute";
 import FeedItem from "@/components/FeedItem";
 import InputField from "@/components/InputField";
 import { colors } from "@/constants";
+import { useCreateComment } from "@/hooks/queries/useCreateComment";
 import { useGetPost } from "@/hooks/queries/useGetPost";
 import { useLocalSearchParams } from "expo-router";
+import { useState } from "react";
 import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function PostDetailScreen() {
   const { id } = useLocalSearchParams();
   const { data: post, isPending, isError } = useGetPost(Number(id));
+  const [content, setContent] = useState("");
+  const createComment = useCreateComment();
 
   if (isError || isPending) {
     return <Text>로딩중</Text>;
   }
+
+  //댓글 생성 요청 핸들러
+  const handleSubmitComment = () => {
+    const commentData = {
+      postId: post.id,
+      content: content,
+    };
+    createComment.mutate(commentData);
+    setContent(""); // 댓글 입력창 초기화
+  };
 
   return (
     <AuthRoute>
@@ -35,8 +49,16 @@ export default function PostDetailScreen() {
           {/* 댓글 인풋 */}
           <View style={styles.commentInputContainer}>
             <InputField
+              value={content}
+              onChangeText={(text) => setContent(text)}
+              returnKeyType='send'
+              onSubmitEditing={handleSubmitComment} // 엔터키를 입력하면 댓글이 바로 등록 되도록 핸들러 연결
+              placeholder='댓글을 남겨 보세요.'
               righteChild={
-                <Pressable style={styles.inputButtonContainer}>
+                <Pressable
+                  disabled={!content} // 입력된 댓글이 없을 때는 클릭되지 않는다.
+                  style={styles.inputButtonContainer}
+                  onPress={handleSubmitComment}>
                   <Text style={styles.inputButtonText}>등록</Text>
                 </Pressable>
               }
