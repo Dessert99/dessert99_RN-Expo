2e859903c821154c3463fa359449ccf6c3b3a0c7
Dessert99 <lsjh1234@naver.com>
2025-10-17 20:13:49 +0900

    feat(screen): handleOpenImagePick 핸들러 연결
    - 카메라 아이콘을 누르면 handleOpenImagePick 작동
    - 앨범이 열리며 사진을 선택하면 배열 형식의result.assets가 나옴
    - getFormDataImages 유틸을 활용해 폼 데이터로 변환한다.
    - useUploadImages훅으로 서버에 이미지 배열을 보낸다.

--- components/PostWriteFooter.tsx
+++ components/PostWriteFooter.tsx
@@ -1,17 +1,42 @@
 import { colors } from "@/constants";
+import { useUploadImages } from "@/hooks/queries/useUploadImages";
+import { getFormDataImages } from "@/utils/image";
 import { Ionicons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 import React from "react";
 import { Pressable, StyleSheet, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 function PostWriteFooter() {
   const inset = useSafeAreaInsets();
+  const uploadImages = useUploadImages();
+
+  // 이미지 피커 핸들러
+  const handleOpenImagePick = async () => {
+    //갤러리 열기
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: "images", //기본값이 images임
+      allowsMultipleSelection: true, // 여러 장 선택 가능
+    });
+
+    //취소를 눌렀다면
+    if (result.canceled) {
+      return;
+    }
+    // 폼 데이터로 변환
+    const formData = getFormDataImages("images", result.assets);
+
+    // 업로드 api
+    uploadImages.mutate(formData, {
+      onSuccess: (data: string[]) => console.log(data),
+    });
+  };
 
   return (
     <View style={[styles.container, { paddingBottom: inset.bottom }]}>
       <Pressable
         style={styles.footerIcon}
-        onPress={{}}>
+        onPress={handleOpenImagePick}>
         <Ionicons
           name={"camera"}
           size={20}
