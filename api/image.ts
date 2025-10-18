5f1801cf0126b50b6f38dd9695ba3ffc297aec76
Dessert99 <lsjh1234@naver.com>
2025-10-18 16:46:32 +0900

    chore: ImagePreviewList 컴포넌트 만들다가 수정
    - uploadImages 요청 로그 위치 수정했다.
    - baseUrls 재사용을 위해 export추가했다.

--- api/image.ts
+++ api/image.ts
@@ -1,12 +1,11 @@
 import { instance } from "./instance";
 
 export const uploadImages = async (body: FormData): Promise<string[]> => {
-  console.log("[API] -> POST /images");
   const { data } = await instance.post("/images", body, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
   });
-
+  console.log("[API] -> POST /images 결과", data);
   return data;
 };
