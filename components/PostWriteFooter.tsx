c053038c1f94aa55d3c5ae0d77c24a6c9f1bc4b8
Dessert99 <lsjh1234@naver.com>
2025-10-18 16:40:57 +0900

    feat(component): 이미지 업로드 성공 후 addImageUris 추가
    - postForm에 이미지 데이터를 전달하는 핸들러이다.
    - 업로드 이미지 개수는 5개로 제한한다.
    - useWatch로 imageUris를 추적하여 이미지를 추가할 때 기존 데이터를 보존한다.
    - setValue로 imageUris 이름의 상태를 업데이트 해준다.

--- components/PostWriteFooter.tsx
+++ components/PostWriteFooter.tsx
@@ -4,12 +4,30 @@ import { getFormDataImages } from "@/utils/image";
 import { Ionicons } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
 import React from "react";
-import { Pressable, StyleSheet, View } from "react-native";
+import { useFormContext, useWatch } from "react-hook-form";
+import { Alert, Pressable, StyleSheet, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 function PostWriteFooter() {
   const inset = useSafeAreaInsets();
   const uploadImages = useUploadImages();
+  const { control, setValue } = useFormContext();
+  const [imageUris] = useWatch({ control, name: ["imageUris"] }); //  name 필드의 값(imageUris)가 바뀔 때마다 컴포넌트 자동으로 리렌더링시켜 최신 값을 가져올 수 있도록 한다.
+
+  // 이미지 데이터 전달 핸들러
+  const addImageUris = (uris: string[]) => {
+    // 업로드 이미지 개수 제한
+    if (imageUris.length + uris.length > 5) {
+      Alert.alert("이미지 개수 초과", "추가 가능한 이미지는 최대 5개입니다.");
+      return;
+    }
+
+    // 이름에 맞게 상태를 업데이트 해준다.
+    setValue(
+      "imageUris",
+      [...imageUris, ...uris.map((uri) => ({ uri: uri }))] // 기존 이미지를 그대로 복사하고 추가
+    );
+  };
 
   // 이미지 피커 핸들러
   const handleOpenImagePick = async () => {
@@ -28,7 +46,7 @@ function PostWriteFooter() {
 
     // 업로드 api
     uploadImages.mutate(formData, {
-      onSuccess: (data: string[]) => console.log(data),
+      onSuccess: (data: string[]) => addImageUris(data),
     });
   };
 
