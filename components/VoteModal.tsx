39be3aebb208f2af93df02924bb9c4ca57b2e4e2
Dessert99 <lsjh1234@naver.com>
2025-10-19 20:47:40 +0900

    feat(modal): 기본적인 투표 모달 구성
    - 투표 아이콘을 클릭하면 setValue를 통해 isVoteOpen의 값이 true로 변한다.
    - VoteModal 컴포넌트가 모달 형식으로 띄워진다.
    - 뒤로가기(헤더)를 누르면 setValue에 의해 isVoteOpen가 false로 변한다.

---
+++ components/VoteModal.tsx
@@ -0,0 +1,63 @@
+import { colors } from "@/constants";
+import { Feather } from "@expo/vector-icons";
+import React from "react";
+import { useFormContext, useWatch } from "react-hook-form";
+import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+function VoteModal() {
+  const { control, setValue } = useFormContext();
+  const [isVoteOpen] = useWatch({ control, name: ["isVoteOpen"] }); // isVoteOpen 상태를 가져온다.
+  return (
+    <Modal
+      visible={isVoteOpen} // true 일 떄만 보인다.
+      animationType='slide'>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <Pressable
+            style={styles.headerLeft}
+            onPress={() => setValue("isVoteOpen", false)} // 뒤로가기 누르면 isVoteOpen이 false가 되며 모달이 내려간다.
+          >
+            <Feather
+              name='arrow-left'
+              size={28}
+              color={colors.BLACK}
+            />
+          </Pressable>
+          <Text style={styles.headerTitle}>투표</Text>
+          <Text style={styles.headerRight}>첨부</Text>
+        </View>
+      </SafeAreaView>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.WHITE,
+    flex: 1,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerLeft: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: colors.BLACK,
+  },
+  headerRight: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    color: colors.ORANGE_600,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default VoteModal;
