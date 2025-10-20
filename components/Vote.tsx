b8ecee0c182bdc8e5d998e088eaa64aa3c7089d0
Dessert99 <lsjh1234@naver.com>
2025-10-20 20:37:46 +0900

    feat(vote): 피드 상세 페이지에서 보이는 Vote컴포넌트 UI 구현
    - postVotes.options에서 투표한 유저 id를 수집한 뒤, auth.id 포함 여부를 통해 내가 투표를 했는지 파악한다.
    - 그 결과인 isVoted값에 따라 투표하기 버튼이 렌더링된다.

---
+++ components/Vote.tsx
@@ -0,0 +1,90 @@
+import { colors } from "@/constants";
+import { useAuth } from "@/hooks/queries/useAuth";
+import { PostVote } from "@/types";
+import { Feather } from "@expo/vector-icons";
+import React, { Fragment } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CustomButton from "./CustomButton";
+
+interface VoteProps {
+  postId: number;
+  postVotes: PostVote[]; // PostVote의 options에 투표 정보가 담긴다. (투표 내용, 어떤 유저가 했는지)
+  voteCount: number;
+}
+
+function Vote({ postId, postVotes, voteCount }: VoteProps) {
+  const { auth } = useAuth();
+  return (
+    <View style={styles.container}>
+      <View style={styles.label}>
+        <Text style={styles.labelTitle}>투표</Text>
+        <View style={styles.labelCount}>
+          <Feather
+            name='user'
+            size={14}
+            color={colors.BLACK}
+          />
+          <Text style={styles.labelCountText}>{voteCount}명 참여</Text>
+        </View>
+      </View>
+
+      {postVotes.map((vote) => {
+        // 투표한 유저 id 수집
+        const voteUsersId = vote.options.flatMap((option) =>
+          option.userVotes.map((userVote) => userVote.userId)
+        );
+
+        // 내가 투표를 했는지
+        const isVoted = voteUsersId.includes(Number(auth.id));
+
+        return (
+          <Fragment key={vote.id}>
+            {vote.options.map((option) => {
+              return <Text key={option.id}>{option.content}</Text>;
+            })}
+
+            {/* 내가 아직 투표를 안했다면 투표하기 버튼 */}
+            {!isVoted && (
+              <CustomButton
+                label='투표하기'
+                onPress={() => {}}
+              />
+            )}
+          </Fragment>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 15,
+    borderWidth: 1,
+    padding: 16,
+    gap: 15,
+    borderRadius: 7,
+    borderColor: colors.GRAY_300,
+  },
+  label: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  labelTitle: {
+    fontSize: 12,
+    fontWeight: "bold",
+    color: colors.ORANGE_600,
+  },
+  labelCount: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 2,
+  },
+  labelCountText: {
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+});
+
+export default Vote;
